7a15e12210c448d74bd83895307d3d1905f4ff10
farhanisty <farhannivtasija@gmail.com>
2024-03-16 23:56:21 +0700

    create MustBeSameValueConstraint class

---
+++ src/custom/constraint/MustBeSameValueConstraint.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Farhanisty\Matrix\Custom\Constraint;
+
+use Farhanisty\Matrix\Engine\AbstractMatrixConstraint;
+use Farhanisty\Matrix\Engine\FailedMatrixConstraintResult;
+use Farhanisty\Matrix\Engine\MatrixConstraintResult;
+
+class MustBeSameValueConstraint extends AbstractMatrixConstraint
+{
+  public function __construct(
+    private int $firstValue,
+    private int $secondValue,
+    protected string $message = "Value is not same"
+  ) { }
+
+  public function check(): MatrixConstraintResult
+  {
+    if($this->firstValue != $this->secondValue) {
+      return new FailedMatrixConstraintResult($this->message);
+    }
+
+    return parent::check();
+  }
+}
