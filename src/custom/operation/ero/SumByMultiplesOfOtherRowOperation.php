f3ac2537fb7be1e5329021f6680b4381bb50866d
farhanisty <farhannivtasija@gmail.com>
2024-03-20 01:01:35 +0700

    create SuByMultiplesOfOtherRowOperation class

---
+++ src/custom/operation/ero/SumByMultiplesOfOtherRowOperation.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Farhanisty\Matrix\Custom\Operation\ERO;
+
+use Farhanisty\Matrix\Custom\Constraint\MustBeNotSameValueConstraint;
+use Farhanisty\Matrix\Custom\Constraint\ValueRangeConstraint;
+use Farhanisty\Matrix\Engine\HasConstraint;
+use Farhanisty\Matrix\Engine\Matrix;
+use Farhanisty\Matrix\Engine\MatrixConstraint;
+use Farhanisty\Matrix\Engine\MatrixResultOperation;
+
+class SumByMultiplesOfOtherRowOperation implements MatrixResultOperation, HasConstraint
+{
+  public function __construct(
+    private int $scalar,
+    private int $otherRow,
+    private int $mainRow,
+    private Matrix $matrix
+  ) {
+  }
+
+  public function getConstraint(): MatrixConstraint
+  {
+    $constraint = new MustBeNotSameValueConstraint(0, $this->scalar);
+    $constraint->setNext(new ValueRangeConstraint(1, $this->matrix->getheight(), $this->otherRow));
+    $constraint->setNext(new ValueRangeConstraint(1, $this->matrix->getheight(), $this->mainRow));
+
+    return $constraint;
+  }
+
+  public function execute(): Matrix
+  {
+    return new Matrix($this->matrix->getHeight(), $this->matrix->getWidth(), $this->getValueResult());
+  }
+
+  public function executeTo(Matrix $matrix): void
+  {
+    $matrix->initialize($this->matrix->getHeight(), $this->matrix->getWidth(), $this->getValueResult());
+  }
+
+  public function getValueResult(): array
+  {
+    $values = $this->matrix->getValues();
+    $result = [];
+
+    $otherRow = $this->matrix->getRow($this->otherRow);
+    $mainRowValues = $this->matrix->getRow($this->mainRow)->getValues();
+
+    foreach ($otherRow->getValues() as $key => $value) {
+      $result[] = ($value * $this->scalar) + $mainRowValues[$key];
+    }
+
+    $values[$this->mainRow - 1] = $result;
+
+    return $values;
+  }
+}
---
+++ tests/custom/operation/ero/SumByMultiplesOfOtherRowOperationTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Farhanisty\Matrix\Custom\Operation\ERO;
+
+use Farhanisty\Matrix\Engine\Matrix;
+use PHPUnit\Framework\TestCase;
+
+class SumByMultiplesOfOtherRowOperationTest extends TestCase
+{
+  public function testExecute()
+  {
+    $matrix = new Matrix(2, 2, [[6, 3], [-1, 4]]);
+
+    $operation = new SumByMultiplesOfOtherRowOperation(2, 2, 1, $matrix);
+    $operation->executeTo($matrix);
+
+    $this->assertSame([4, 11], $matrix->getRow(1)->getValues());
+
+    $operation = new SumByMultiplesOfOtherRowOperation(-1, 1, 2, $matrix);
+    $operation->executeTo($matrix);
+
+    $this->assertSame([-5, -7], $matrix->getRow(2)->getValues());
+  }
+}
