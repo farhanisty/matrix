c0ac197f3ae191b8d01b29e18da6546a4f0a9bbd
farhanisty <farhannivtasija@gmail.com>
2024-03-20 00:11:51 +0700

    create MustBeNotSameValueConstraint class

---
+++ src/custom/constraint/MustBeNotSameValueConstraint.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Farhanisty\Matrix\Custom\Constraint;
+
+use Farhanisty\Matrix\Engine\AbstractMatrixConstraint;
+use Farhanisty\Matrix\Engine\MatrixConstraintResult;
+
+class MustBeNotSameValueConstraint extends AbstractMatrixConstraint
+{
+  public function __construct(
+    private $firstValue,
+    private $secondValue
+  ) {
+  }
+
+  public function check(): MatrixConstraintResult
+  {
+    if ($this->firstValue !== $this->secondValue) {
+      return FailedMatrixConstraintResult("Both value must be not same");
+    }
+
+    return parent::check();
+  }
+}
---
+++ tests/custom/constraint/MustBeNotSameValueConstraintTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Farhanisty\Matrix\Custom\Constraint;
+
+use PHPUnit\Framework\TestCase;
+
+class MustBeNotSameValueConstraintTest extends TestCase
+{
+  public function testCheck()
+  {
+    $constraint = new MustBeSameValueConstraint(1, 2);
+    $result = $constraint->check();
+    $this->assertFalse($result->getStatus());
+
+    $constraint = new MustBeSameValueConstraint(5, 5);
+    $result = $constraint->check();
+    $this->assertTrue($result->getStatus());
+  }
+}
